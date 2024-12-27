cb9ea2078fe7c6acbf5ad024b8ea0ccb66608989
vitalii-pc <alievruslan1818@gmail.com>
2024-12-27 19:39:32 +0100

    add_courses

--- src/components/TrainingCourses/TrainingSection/TrainingSection.tsx
+++ src/components/TrainingCourses/TrainingSection/TrainingSection.tsx
@@ -1,52 +1,45 @@
+import React from 'react';
 import s from './TrainingSection.module.css';
 
-export default function TrainingSection() {
+type TrainingItem = {
+  title: string;
+  text: string;
+};
+
+type TrainingProps = {
+  titleHead: string;
+  sections: TrainingItem[];
+  button: string;
+};
+
+export default function TrainingSection({
+  sections,
+  titleHead,
+  button
+}: TrainingProps) {
   return (
     <div className={s.training__section}>
-      <h2 className={s.training__section_title}>
-        День 1 <br /> Теоретична частина
-      </h2>
-      <ul className={s.training__section_list}>
-        <li className={s.training__section_item}>
-          <p className={s.training__section_text_head}>
-            Вступ: Основи професії
-          </p>
-          <p className={s.training__section_text}>
-            Що таке нарощування вій, основні Огляд сучасних трендів (1:1, 2D-9D,
-            «Kim Kardashian», мокрий ефект)
-          </p>
-        </li>
-        <li className={s.training__section_item}>
-          <p className={s.training__section_text_head}>Гігієна та безпека</p>
-          <p className={s.training__section_text}>
-            Санітарні норми та стерилі Правила роботи з клієнтами для збереження
-          </p>
-        </li>
-        <li className={s.training__section_item}>
-          <p className={s.training__section_text_head}>Анатомія вій та очей</p>
-          <p className={s.training__section_text}>
-            Типи вій: природні характеристики та будов Як визначати оптимальний
-            стиль нарощування залежно від форми очей.
-          </p>
-        </li>
-        <li className={s.training__section_item}>
-          <p className={s.training__section_text_head}>
-            Матеріали та інструменти
-          </p>
-          <p className={s.training__section_text}>
-            Огляд інструментів: пінцети, клей, підкладки, щіточки. Типи штучних
-            вій: товщина, вигини, довжина, матеріал.
-          </p>
-        </li>
-        <li className={s.training__section_item}>
-          <p className={s.training__section_text_head}>Робоче місце майстра</p>
-          <p className={s.training__section_text}>
-            Як організувати комфортне та ергономічне місце для роботи.
-            Психологія спілкування з клієнтами.
-          </p>
-        </li>
-      </ul>
-      <button className={s.training__section_btn}>Записатись</button>
+      <div>
+        <h2 className={s.training__section_title}>
+          {titleHead.split('\n').map((line, index) => (
+            <React.Fragment key={index}>
+              {line}
+              <br />
+            </React.Fragment>
+          ))}
+        </h2>
+        <ul className={s.training__section_list}>
+          {sections.map((section, index) => {
+            return (
+              <li key={index} className={s.training__section_item}>
+                <p className={s.training__section_text_head}>{section.title}</p>
+                <p className={s.training__section_text}>{section.text}</p>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      <button className={s.training__section_btn}>{button}</button>
     </div>
   );
 }
