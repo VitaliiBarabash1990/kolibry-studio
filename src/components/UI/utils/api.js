76c3c44b24bf4f7a9193efdae7c4ca04e5dddab7
vitalii-pc <alievruslan1818@gmail.com>
2024-12-30 22:48:06 +0100

    add_server

--- src/components/UI/utils/api.js
+++ src/components/UI/utils/api.js
@@ -3,7 +3,10 @@ import toast from 'react-hot-toast';
 
 export const sendMail = async (body) => {
   try {
-    await axios.post('http://localhost:5000/send-email', body);
+    await axios.post(
+      'https://kolibry-studio-back.onrender.com/send-email',
+      body
+    );
     toast.success('Сообщение отправлено!');
   } catch (error) {
     console.error('Ошибка отправки:', error);
